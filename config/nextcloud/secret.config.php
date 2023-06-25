bd3a01e810694fe949438afd9a5f03ecc341cef3
pli01 <pli01@users.noreply.github.com>
2023-06-25 14:48:00 +0200

    default config

--- config/nextcloud/secret.config.php
+++ config/nextcloud/secret.config.php
@@ -2,4 +2,7 @@
 $CONFIG = array (
   'passwordsalt' => getenv('NC_CONFIG_PASSWORDSALT'),
   'secret' => getenv('NC_CONFIG_SECRET'),
+  'skeletondirectory' => '',
+  'templatedirectory' => '',
+  'knowledgebaseenabled' => false
 );
