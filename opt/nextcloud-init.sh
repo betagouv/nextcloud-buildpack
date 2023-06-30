ed9c4dadc183e8677908797ec335bc594a90c70f
pli01 <pli01@users.noreply.github.com>
2023-06-30 10:26:31 +0200

    apcu

--- opt/nextcloud-init.sh
+++ opt/nextcloud-init.sh
@@ -198,4 +198,5 @@ php_conf_dir="vendor/php/etc/conf.d/"
 erb $basedir/conf/php/php-pgsql.ini.erb > ${php_conf_dir}/php-pgsql.ini
 erb $basedir/conf/php/php-redis-session.ini.erb > ${php_conf_dir}/php-redis-session.ini
 erb $basedir/conf/php/php-opcache.ini.erb > ${php_conf_dir}/php-opcache.ini
-
+erb $basedir/conf/php/php-apcu.ini.erb > ${php_conf_dir}/php-apcu.ini
+echo "# End init"
