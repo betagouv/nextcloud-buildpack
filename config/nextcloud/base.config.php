0d9522311b245cf49a02348745323ac830786fd1
Denis B <denis.baudot@gmail.com>
2024-02-23 14:17:32 +0100

    add version

--- config/nextcloud/base.config.php
+++ config/nextcloud/base.config.php
@@ -14,6 +14,7 @@ $CONFIG = array (
   'loglevel' => getenv('NC_CONFIG_LOGLEVEL') ?: 2,
   'loglevel_frontend' => getenv('NC_CONFIG_LOGLEVEL_FRONTEND') ?: 2,
   'instanceid' => getenv('NC_INSTANCEID'),
+  'version' => getenv('NC_VERSION'),
 );
 
 $trustedDomains = getenv('NC_TRUSTED_DOMAINS');
