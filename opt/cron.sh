a8830f8341824a38bada1d532bfcb31143c7ff0a
Denis B <denis.baudot@gmail.com>
2024-02-23 14:36:12 +0100

    start nginx+php for expend memory_limit from php.ini

--- opt/cron.sh
+++ opt/cron.sh
@@ -9,4 +9,16 @@ fi
 echo "# Init nextcloud"
 bin/nextcloud-config.sh
 
+#
+# init nginx config
+#
+echo "# Init nginx nextcloud config"
+erb conf/nginx/nextcloud.conf.erb > conf/nginx/nextcloud.conf
+export NGINX_HTTP_INCLUDES="conf/nginx/nextcloud.conf"
+#
+# start php+nginx script (from https://github.com/Scalingo/php-buildpack/blob/master/bin/compile)
+#
+echo "# Start nginx+php"
+bin/run
+
 php -f nextcloud/cron.php
