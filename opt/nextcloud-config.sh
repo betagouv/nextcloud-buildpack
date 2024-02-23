048093f6930ae11c6babaf262c7465bba027aeec
Denis B <denis.baudot@gmail.com>
2024-02-23 11:45:50 +0100

    enable file_external, launch php occ upgrade, add more memory

--- opt/nextcloud-config.sh
+++ opt/nextcloud-config.sh
@@ -77,3 +77,17 @@ for c in $NC_CONFIG_TEMPLATE; do
 done
 fi
 )
+
+
+#
+# app
+#
+if [[ -z "$NC_APP_ENABLE" ]]; then
+  NC_APP_ENABLE="files_external"
+fi
+
+for app in ${NC_APP_ENABLE}; do
+  php occ app:enable $app
+done
+
+php occ upgrade
