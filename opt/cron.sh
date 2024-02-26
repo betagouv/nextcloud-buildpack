dc933d4d51233e1885c745517b713fcdc7f5742a
Denis B <denis.baudot@gmail.com>
2024-02-26 15:39:19 +0100

    remove nginx launch for cron, try memory limit in command line

--- opt/cron.sh
+++ opt/cron.sh
@@ -9,16 +9,4 @@ fi
 echo "# Init nextcloud"
 bin/nextcloud-config.sh
 
-#
-# init nginx config
-#
-echo "# Init nginx nextcloud config"
-erb conf/nginx/nextcloud.conf.erb > conf/nginx/nextcloud.conf
-export NGINX_HTTP_INCLUDES="conf/nginx/nextcloud.conf"
-#
-# start php+nginx script (from https://github.com/Scalingo/php-buildpack/blob/master/bin/compile)
-#
-echo "# Start nginx+php"
-bin/run
-
-php -f nextcloud/cron.php
+php -d memory_limit=512M nextcloud/cron.php
