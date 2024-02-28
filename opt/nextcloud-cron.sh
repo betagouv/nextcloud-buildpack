f15ce372bbfc222f3cddfb49f42e948f10a4b278
Denis B <denis.baudot@gmail.com>
2024-02-28 17:53:35 +0100

    fix cron

--- opt/nextcloud-cron.sh
+++ opt/nextcloud-cron.sh
@@ -3,17 +3,12 @@
 if [ -n "$DEBUG" ]; then
   set -x
 fi
+
 #
-# init php with includes
+# init nextcloud config
 #
-basedir="/app"
-echo "# prepare includes php ini"
-php_conf_dir="vendor/php/etc/conf.d/"
-erb $basedir/conf/php/php-pgsql.ini.erb > ${php_conf_dir}/php-pgsql.ini
-erb $basedir/conf/php/php-redis-session.ini.erb > ${php_conf_dir}/php-redis-session.ini
-erb $basedir/conf/php/php-opcache.ini.erb > ${php_conf_dir}/php-opcache.ini
-erb $basedir/conf/php/php-apcu.ini.erb > ${php_conf_dir}/php-apcu.ini
-echo "# End init"
+echo "# Init nextcloud"
+bin/nextcloud-run.sh
 
 
 php -f nextcloud/cron.php
